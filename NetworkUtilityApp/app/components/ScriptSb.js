16127a0755deb07ee9f00163ec02ac30ce0b5b14
PKrasner <pavel.krasner@computacenter.com>
2019-02-27 10:17:46 +0100

    python execution initial

--- NetworkUtilityApp/app/components/ScriptSb.js
+++ NetworkUtilityApp/app/components/ScriptSb.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
+import { debug } from 'electron-log';
+
+var PythonService = require('../services/PythonService')
 
 type Props = {};
 
@@ -10,6 +13,11 @@ export default class ScriptSb extends Component<Props> {
   props: Props;
 
   render() {
+
+    PythonService.ExecuteScript('test.py', (r) => {
+      debugger;
+    });
+
     return (
       <div className={styles.container} data-tid="container">
         <h2>New mega page</h2>
---
+++ NetworkUtilityApp/app/services/PythonService.js
@@ -0,0 +1,24 @@
+var pShell = require('python-shell')
+debugger;
+
+export var ExecuteScript = function (name, callback) {
+
+  let options = {
+    mode: 'text',
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: './python',
+    args: []
+  };
+
+  let pyshell = new pShell.PythonShell('./python/' + name);
+
+  // sends a message to the Python script via stdin
+  pyshell.send('');
+
+  pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    console.log(message);
+    debugger;
+    callback(message);
+  });
+}
